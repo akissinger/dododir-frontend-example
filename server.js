bd71affef1962cdc958586f7ebb118f43e3f4fb4
Aleks Kissinger <aleks0@gmail.com>
2026-06-08 17:54:54 +0100

    brought env over

--- server.js
+++ server.js
@@ -4,15 +4,12 @@ import { fileURLToPath } from 'url';
 import { DododirServer } from 'dododir';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const PORT = Number(process.env.PORT ?? 3001);
 
 const server = new DododirServer({
-    port: PORT,
-    staticDir: path.join(__dirname, '../../client/dist'),
-    staticCatchall: path.join(__dirname, '../../client/dist/index.html'),
-    allowOrigin: 'http://localhost:5173',
-    dbPath: process.env.DB_PATH ?? path.join(__dirname, '../../dododir.db'),
-    userQuotaMB: Number(process.env.USER_QUOTA_MB ?? 100),
+    port: 3001,
+    staticDir: path.join(__dirname, 'dist'),
+    staticCatchall: path.join(__dirname, 'dist/index.html'),
+    dbPath: path.join(__dirname, 'dododir.db'),
     authSecret: process.env.JWT_SECRET,
     turnstileSecretKey: process.env.TURNSTILE_SECRET_KEY,
 });
